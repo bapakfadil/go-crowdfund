a2b3dfa37dc91047b7e425b19193eab27d03a237
bapakfadil <sipadil28@hotmail.com>
2025-02-23 21:47:54 +0700

    Created Login endpoint

--- handler/user.go
+++ handler/user.go
@@ -8,20 +8,23 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// struct for user handler
 type userHandler struct {
 	userService user.Service
 }
 
+// Function to create new user handler
 func NewUserHandler(userService user.Service) *userHandler {
 	return &userHandler{userService}
 }
 
+// Function to handle register user
 func (h *userHandler) RegisterUser(c *gin.Context) {
-	// tangkap input dari user
-	// map input dari user ke struct RegisterUserInput
-	// struct di atas kita panggil di service
-	// service akan memanggil repository
-	// repository untuk simpan data user ke db
+	// 1. tangkap input dari user
+	// 2. map input dari user ke struct RegisterUserInput
+	// 3. struct di atas kita panggil di service
+	// 4. service akan memanggil repository
+	// 5. repository untuk simpan data user ke db
 
 	var input user.RegisterUserInput
 
@@ -51,5 +54,42 @@ func (h *userHandler) RegisterUser(c *gin.Context) {
 	formatter := user.FormatUser(newUser, "JWT_TOKEN")
 	response := helper.APIResponse("Account has been registered", http.StatusOK, "Success", formatter)
 	
+	c.JSON(http.StatusOK, response)
+}
+
+// Function to handle login
+func (h *userHandler) Login(c *gin.Context) {
+	// 1. user memasukkan input (email dan password)
+	// 2. input ditangkap handler
+	// 3. mapping dari input user ke input struct
+	// 4. input struct passing ke service
+	// 5. di service mencari dengan bantuan repository user dengan email x
+	// 6. mencocokan password
+
+	var input user.LoginInput
+
+	err := c.ShouldBindJSON(&input)
+	if err != nil {
+		errors := helper.FormatValidationError(err)
+		errorMessage := gin.H{"errors":errors}
+
+		response := helper.APIResponse("Login failed", http.StatusUnprocessableEntity, "Error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	loggedInUser, err := h.userService.Login(input)
+
+	if err != nil {
+		errorMessage := gin.H{"errors": err.Error()}
+
+		response := helper.APIResponse("Login failed", http.StatusUnprocessableEntity, "Error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	formatter := user.FormatUser(loggedInUser, "JWT_TOKEN")
+	response := helper.APIResponse("Successfully logged in", http.StatusOK, "Success", formatter)
+
 	c.JSON(http.StatusOK, response)
 }
--- user/repository.go
+++ user/repository.go
@@ -6,25 +6,40 @@ import "gorm.io/gorm"
 type Repository interface {
 	// Save is a function to save user data to the database
 	Save(user User) (User, error)
+	FindByEmail(email string) (User, error)
 }
 
-// repository is a struct to define the repository
+// repository is an object to connect to the database
 type repository struct {
 	// db is an instance of gorm.DB
 	db *gorm.DB
 }
 
-// NewRepository is a constructor
+// Function to create a new repository
 func NewRepository(db *gorm.DB) *repository {
   return &repository{db}
 }
 
-// r *repository is a receiver
+// Function to save user data to the database
 func (r *repository) Save(user User) (User, error) {
 	// Create user data to the database
 	err := r.db.Create(&user).Error
 	
 	// If there is an error, return the error
+	if err != nil {
+		return user, err
+	}
+
+	return user, nil
+}
+
+// Function to find user data by email
+func (r *repository) FindByEmail(email string) (User, error) {
+	var user User
+
+	// Find user data by email
+	err := r.db.Where("email = ?", email).Find(&user).Error
+
 	if err != nil {
 		return user, err
 	}
